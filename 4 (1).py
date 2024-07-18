dff828e84e3803d00300692df75007a39c071b19
Gabriel Soares <gabriel49930366@gmail.com>
2024-07-18 11:01:22 -0400

    new test

---
+++ 4 (1).py
@@ -0,0 +1,8 @@
+x=float(input("Digite um numero: "))
+xx=float(input("Digite outro numero: "))
+y=float(input("Digite outro numero: "))
+
+if x<xx<y:
+    print("Essa sequencia esta em ordem crescente.")
+else:
+    print("Essa sequencia nao esta em ordem crescente.")
